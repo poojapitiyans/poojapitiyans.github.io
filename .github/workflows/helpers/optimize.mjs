23500e71f73871c59393b3cee2d08304d33c5bd5
damith <damith@corzent.com>
2023-10-20 12:39:53 +0530

    Improve

--- .github/workflows/helpers/optimize.mjs
+++ .github/workflows/helpers/optimize.mjs
@@ -1,16 +1,40 @@
 import imagemin from 'imagemin';
-import imageminPngquant from 'imagemin-pngquant';
 import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngquant from 'imagemin-pngquant';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+// Define the paths to seek out images
+const paths = [
+  'images',
+  'css',
+  'js',
+  'fonts'
+];
 
 (async () => {
-    await imagemin(['images/*.{jpg,png}'], {
-        destination: 'optimized_images',
+  for (const path of paths) {
+    // Discover the names of image files dwelling within the path
+    const files = (await fs.readdir(path)).filter(file => file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.jpeg'));
+    
+    for (const file of files) {
+      // Define the full path to the image
+      const filePath = join(path, file);
+      
+      console.log(`Optimizing ${filePath}...`);
+      
+      // Optimize the image and replace the original
+      await imagemin([filePath], {
+        destination: path,
         plugins: [
-            imageminMozjpeg({ quality: [0.6, 0.8] }),
-            imageminPngquant({
-                quality: [0.6, 0.8]
-            })
+          imageminMozjpeg({ quality: 80 }),
+          imageminPngquant({ quality: [0.6, 0.8] })
         ]
-    });
-    console.log('Images optimized');
+      });
+      
+      console.log(`Optimized ${filePath}`);
+    }
+  }
+  
+  console.log('All images optimized');
 })();
